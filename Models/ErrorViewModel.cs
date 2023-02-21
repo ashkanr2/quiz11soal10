d8f1ba7f909a0d7d93eee01cc49fdcd2c3922825
ashkanr2 <100090656+ashkanr2@users.noreply.github.com>
2023-02-21 18:20:58 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace quiz11.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
